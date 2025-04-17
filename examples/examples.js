36dfbbfdf31bdf3c927a03d296e3635686171c2e
Myndex <perceptex@myndex.com>
2025-04-16 22:55:42 -0700

    Update examples.js

--- examples/examples.js
+++ examples/examples.js
@@ -5,11 +5,11 @@ const LEVEL_LABELS = ['REJECT', 'AA L', 'AA', 'AAA', 'AAA'];
 const FONT_SIZE = ['32px', '24px', '18.7px', '16px', '12px'];
 const PADDING = ['0.1em 0.25em', '0.33em', '0.8em', '1em', '1.1em'];
 const APCA_LABELS = [ //[0, 15,     22,      30,     37.5,     45,     52.5,    60,      67.5,    75,     82.5,     90,  110];
-		['REJECT','NO TXT','NO TXT','SPOT-B','SUBFL1','FLUENT','FLUENT','BODYMN','BODYTX','BODYTX','BODYTX'], // 32px
-		['REJECT','NO TXT','NO TXT','SPOT-A','SUBFL1','FLUENT','FLUENT','BODYMN','BODYTX','BODYTX','BODYTX'], // 24px
-		['REJECT','NO TXT','NO TXT','SPOT-B','SUBFL1','FLUENT','FLUENT','BODYMN','BODYTX','BODYTX','BODYTX'], // 18.7px
-		['REJECT','NO TXT','NO TXT','SPOT-B','SUBFL1','SUBFL2','FLUENT','BODYMN','BODYTX','BODYTX','BODYTX'], // 16px
-		['REJECT','NO TXT','NO TXT','SPOT-B','SUBFL1','SUBFL2','FLUENT','FLUENT','FLUENT','FLUENT','FLUENT'], // 12px
+		['REJECT','NO TXT','NO TXT','SPOT-B','SUBFL1','FLUENT','FLUENT','BODYMN','BODYTX','BODYTX','TOO-HI'], // 32px
+		['REJECT','NO TXT','NO TXT','SPOT-A','SPOT-B','SUBFL1','FLUENT','BODYMN','BODYMN','BODYTX','BODYTX'], // 24px
+		['REJECT','NO TXT','NO TXT','SPOT-A','SPOT-B','SUBFL1','FLUENT','FLUENT','FLUENT','BODYMN','BODYTX'], // 18.7px
+		['REJECT','REJECT','REJECT','SPOT-A','SPOT-B','SUBFL2','SUBFL2','FLUENT','FLUENT','BODYMN','BODYTX'], // 16px
+		['REJECT','REJECT','REJECT','NO TXT','NO TXT','SUBFL2','SUBFL2','SUBFL2','SUBFL2','SUBFL2','SUBFL2'], // 12px
 	];
 
 // [0,15,22,30,37.5,45,52.5,60,67.5,75,82.5,90,110];
@@ -35,8 +35,6 @@ const COLORS = [
 	['#bbbbbb', '#000000', '#ffffff'],
 	['#cccccc', '#000000', '#ffffff'],
 	['#dddddd', '#000000', '#ffffff'],
-	['#eeeeee', '#000000', '#ffffff'],
-
 	['#9999ff', '#000000', '#ffffff'],
 	['#00bf00', '#000000', '#ffffff'],
 	['#ff7373', '#000000', '#ffffff'],
@@ -100,7 +98,7 @@ var addExample = function(fg, bg) {
 
 	var apca_contrast = apca.contrast(cfg, cbg);
 	var apca_level = getLevel(apca_contrast, apca);
-	apca_level = Math.max(0, apca_level - offset);
+	// apca_level = Math.max(0, apca_level - offset);
 	
 	clone.querySelector('.apca output').textContent = apca_contrast.toFixed(1);
 	clone.querySelector('.apca .badge').textContent = APCA_LABELS[wcag_level][apca_level];
